8f0bd364d5e33c6ef096513c22b6b512f208989a
wei zheng <weijie0191@gmail.com>
2020-08-19 17:31:03 -0400

    update

--- server/CoreServer/CoreServer/Program.cs
+++ server/CoreServer/CoreServer/Program.cs
@@ -19,6 +19,8 @@ namespace CoreServer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://*:19988");
     }
 }
